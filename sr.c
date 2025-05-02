b91e98f4e84f4cbbdeca518986143cef4a51d804
AnkingLau <132275171+AnkingLau@users.noreply.github.com>
2025-05-02 21:44:19 +0930

    Update sr.c
    1.Final testing passed with all expected behaviors in emulator.
    2.Cleaned up code, added comments, and confirmed submission format.

--- sr.c
+++ sr.c
@@ -1,5 +1,7 @@
 /*
  * sr.c - Selective Repeat Protocol Implementation
+ * Created: 2025-04-01
+ * Last Modified: 2025-05-02
  * StudentID:a1820379
  * Description: Selective Repeat protocol implementation adapted from Go-Back-N
  */
@@ -10,15 +12,17 @@
  #include "emulator.h"
  #include "sr.h"
  
- #define RTT  16.0 // 2025-04-01: Set round trip timeout value
- #define WINDOWSIZE 6 //  Set send/receive window size
- #define SEQSPACE 12  //  Sequence space must be >= 2 * WINDOWSIZE
+ // Set protocol constants
+ #define RTT  16.0
+ #define WINDOWSIZE 6
+ #define SEQSPACE 12
  #define NOTINUSE (-1)
  
- //  Compute checksum for a packet
+ // Compute checksum for a packet
  int ComputeChecksum(struct pkt packet) {
      int checksum = packet.seqnum + packet.acknum;
-     for (int i = 0; i < 20; i++) {
+     int i;
+     for (i = 0; i < 20; i++) {
          checksum += (int)(packet.payload[i]);
      }
      return checksum;
@@ -32,7 +36,6 @@
  // Sender variables
  static struct pkt send_buffer[SEQSPACE];
  static bool acked[SEQSPACE];
- static float timers[SEQSPACE];
  static int base = 0;
  static int nextseqnum = 0;
  
@@ -41,13 +44,14 @@
  static bool received[SEQSPACE];
  static int expected_base = 0;
  
- // 2025-04-05 Handle message from layer 5 at sender
+ // Handle message from layer 5 at sender
  void A_output(struct msg message) {
      if ((nextseqnum + SEQSPACE - base) % SEQSPACE < WINDOWSIZE) {
          struct pkt packet;
+         int i;
          packet.seqnum = nextseqnum;
          packet.acknum = NOTINUSE;
-         for (int i = 0; i < 20; i++) packet.payload[i] = message.data[i];
+         for (i = 0; i < 20; i++) packet.payload[i] = message.data[i];
          packet.checksum = ComputeChecksum(packet);
  
          send_buffer[nextseqnum] = packet;
@@ -66,7 +70,7 @@
      }
  }
  
- // 2025-04-10: Handle incoming ACK at the sender
+ // Handle incoming ACK at sender
  void A_input(struct pkt packet) {
      if (!IsCorrupted(packet) && acked[packet.acknum] == false) {
          acked[packet.acknum] = true;
@@ -85,12 +89,13 @@
      }
  }
  
- // 2025-04-15: Timeout handler at sender
+ // Timeout handler at sender
  void A_timerinterrupt(void) {
+     int i;
      if (TRACE > 0)
          printf("A: Timeout occurred, resending all unACKed packets\n");
  
-     for (int i = 0; i < WINDOWSIZE; i++) {
+     for (i = 0; i < WINDOWSIZE; i++) {
          int seq = (base + i) % SEQSPACE;
          if (!acked[seq]) {
              tolayer3(A, send_buffer[seq]);
@@ -101,18 +106,20 @@
      starttimer(A, RTT);
  }
  
- // 2025-04-15: Initialize sender state
+ // Initialize sender state
  void A_init(void) {
-     for (int i = 0; i < SEQSPACE; i++) {
+     int i;
+     for (i = 0; i < SEQSPACE; i++) {
          acked[i] = false;
      }
      base = 0;
      nextseqnum = 0;
  }
  
- // 2025-04-12: Handle packet at receiver
+ // Handle packet at receiver
  void B_input(struct pkt packet) {
      struct pkt ackpkt;
+     int i;
      if (!IsCorrupted(packet)) {
          if ((packet.seqnum >= expected_base && packet.seqnum < expected_base + WINDOWSIZE) ||
              (expected_base + WINDOWSIZE >= SEQSPACE &&
@@ -139,12 +146,12 @@
  
      ackpkt.seqnum = 0;
      ackpkt.acknum = packet.seqnum;
-     for (int i = 0; i < 20; i++) ackpkt.payload[i] = '0';
+     for (i = 0; i < 20; i++) ackpkt.payload[i] = '0';
      ackpkt.checksum = ComputeChecksum(ackpkt);
      tolayer3(B, ackpkt);
  }
  
- // 2 May Initialize receiver state
+ // Initialize receiver state
  void B_init(void) {
      int i;
      expected_base = 0;
@@ -160,5 +167,4 @@
  void B_timerinterrupt(void) {
      // Not needed for unidirectional transfer
  }
- 
  
